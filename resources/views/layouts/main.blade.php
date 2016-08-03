ad5194551a56de5e33cc68841d8abd12fdb75166
ChuanHong <ChuanHong>
2016-08-03 16:15:02 +0800

    blade樣板引擎，樣板的繼承，父視圖 子視圖

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+
+<html>
+<head>
+    <title>@yield("title")</title>
+</head>
+
+<body>
+    @section("sidebar")
+        這是父視圖側邊攔.
+    @show
+    
+    <div class="container">
+        @yield("content_1")
+    </div>
+    
+    
+    
+    
+    
+</body>
+</html>
+    
+
+    
+    
