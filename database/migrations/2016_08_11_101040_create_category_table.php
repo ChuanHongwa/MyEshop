45bc2b90c03d10b41fc9750ad0c4e6ab668332f7
ChuanHong <ChuanHong>
2016-08-11 11:23:46 +0800

    建立產品，目錄，文章，品牌的資料表 ，安裝2個套件

---
+++ database/migrations/2016_08_11_101040_create_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("name", 60);
+            $table->string('created_at_ip');
+            $table->string('updated_at_ip');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('category');
+    }
+}
